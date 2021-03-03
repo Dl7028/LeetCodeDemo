0e643d068c8b756cbf4f7f6f180160665d965c4e
Yuki-r <282107958@qq.com>
2021-03-03 20:39:40 +0800

    feat(Offer26): 剑指 Offer 26. 树的子结构

--- leetCode/src/main/java/com/yks/leetcode/剑指offer/Offer26.java
+++ leetCode/src/main/java/com/yks/leetcode/剑指offer/Offer26.java
@@ -3,9 +3,10 @@ package com.yks.leetcode.剑指offer;
 /**
  * @Description: 剑指 Offer 26. 树的子结构
  * @Author: Yu ki-r
- * @CreateDate: 2021/1/30 15:01
+ * @CreateDate: 2021/3/3 19:58
  */
 public class Offer26 {
+
     /**
      * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
      *
@@ -41,11 +42,46 @@ public class Offer26 {
      */
 
      public class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
- }
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+
+    /**
+     * 第一步，找出树A中与树B根节点一样的节点R（递归）
+     * 第二步，比较R节点与B节点左右节点是否相等（递归）
+     * @param A
+     * @param B
+     * @return
+     */
+    public boolean compareTreeVal(TreeNode A, TreeNode B) { //第二步，比较相等的根节点下的子树是否相等
+        if (B==null)
+            return true;
+        if (A==null) {
+            return false;
+        }
+        if (A.val!=B.val)
+            return false;
+        return compareTreeVal(A.left,B.left)&&compareTreeVal(A.right,B.right);
+    }
 
 
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) { //第1步,比较根节点是否相等
+        boolean result = false;
+        if (A!=null&&B!=null) {
+
+            if (A.val == B.val) {
+                result = compareTreeVal(A, B);
+            }
+            if (!result) {
+                result = isSubStructure(A.right, B);
+            }
+            if (!result) {
+                result = isSubStructure(A.left, B);
+            }
+        }
+        return  result;
+    }
 }
